e4f5b51e558940616f1eff02d4fb67a04b0b3369
schuyler <sasplin@ewu.edu>
2022-05-31 23:01:58 -0700

    dailyLeaderBoard style adjustments

--- wordle-web/components/day-stats.vue
+++ wordle-web/components/day-stats.vue
@@ -1,41 +1,51 @@
 <template>
-  <v-row>
-      <v-card-title>Day stats for: {{ new Date(dateStats.day).toDateString() }}</v-card-title>
-      <v-card-text>
-        <v-row>
-          <v-col> Plays</v-col>
-          <v-col> Avg. Attempts</v-col>
-          <v-col> Avg. Time</v-col>
-          <v-col> Win Rate</v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            {{ dateStats.plays }}
-          </v-col>
-          <v-col>
-            {{ dateStats.averageGuesses }}
-          </v-col>
-          <v-col>
-            {{ dateStats.averageTime }}
-          </v-col>
-          <v-col>
-            {{ dateStats.winRate }}
-          </v-col>
-        </v-row>
-      </v-card-text>
+  <v-row class="mt-3">
+    <v-card-title
+      >Day stats for: {{ new Date(dateStats.day).toDateString() }}</v-card-title
+    >
+    <v-card-text>
+      <v-row>
+        <v-col cols="3">
+          {{ dateStats.plays }}
+        </v-col>
+        <v-col cols="3">
+          {{ dateStats.averageGuesses }}
+        </v-col>
+        <v-col cols="3">
+          {{ formattedTime }}
+        </v-col>
+        <v-col cols="3">
+          {{ dateStats.winRate }}
+        </v-col>
+      </v-row>
+    </v-card-text>
   </v-row>
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue} from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator'
 
-@Component({components: {}})
+@Component({ components: {} })
 export default class DayStats extends Vue {
-  @Prop({required: true})
-  dateStats!: undefined
+  @Prop({ required: true })
+  dateStats: any
 
-  mounted(){
+  mounted() {
     console.log(this.dateStats)
+  }
+
+  get formattedTime() {
+    // Fun time formatting things
+    return (
+      (this.dateStats.averageTime / 60 / 60 > 1
+        ? Math.floor(this.dateStats.averageTime / 60 / 60) + ':'
+        : '') + // Hours or nothing
+      (Math.floor(this.dateStats.averageTime / 60) % 60) + // Minutes
+      ':' + // minute divider
+      (Math.floor(this.dateStats.averageTime % 60) < 10
+        ? '0' + Math.floor(this.dateStats.averageTime % 60)
+        : Math.floor(this.dateStats.averageTime % 60))
+    ) // Seconds
   }
 
   // playerStatsDisplay: any = {
@@ -48,6 +58,5 @@ export default class DayStats extends Vue {
 
   // thisDaysGameWon() {
   // if (this.practiceMode === false && this.wasPlayed === true) {
-
 }
 </script>
--- wordle-web/pages/dailyLeaderboard.vue
+++ wordle-web/pages/dailyLeaderboard.vue
@@ -6,15 +6,21 @@
           <v-card-title class="justify-center">
             You're being exploited for ad revenue, please standby...
           </v-card-title>
-          <PrerollAd/>
+          <PrerollAd />
         </v-card>
       </v-row>
     </v-container>
     <v-container v-else fill-height justify-center>
-      <v-card>
+      <v-card class="pa-6">
         <v-card-title class="display-3 justify-center">
           Leader Board
         </v-card-title>
+        <v-row>
+          <v-col> Plays</v-col>
+          <v-col> Avg. Attempts</v-col>
+          <v-col> Avg. Time</v-col>
+          <v-col> Win Rate</v-col>
+        </v-row>
         <v-container>
           <v-row v-for="(dayStats, index) in recentStats" :key="index">
             <v-card>
@@ -28,14 +34,14 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class PrevDayStats extends Vue {
-  recentStats: any | null = null;
+  recentStats: any | null = null
 
   playerGuid: string = ''
-  showStats: boolean = false;
+  showStats: boolean = false
 
   mounted() {
     const playerGUID = localStorage.getItem('playerGUID')
@@ -48,13 +54,14 @@ export default class PrevDayStats extends Vue {
     console.log(this.playerGuid)
     this.$axios
       .post('/api/RecentStats', {
-        guid: this.playerGuid
+        guid: this.playerGuid,
       })
       .then((response) => {
         this.recentStats = response.data
-        this.showStats = true;
+        this.showStats = true
         // return response.data
-      }).catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error))
     // this.getData();
   }
 
@@ -76,5 +83,3 @@ class Guid {
   }
 }
 </script>
-
-
